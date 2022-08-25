42e5eeb50ba3411c348ed17f02616c1184b6d5cc
Cubes Endurance <ec@sapphirepack.org>
2022-08-24 19:56:57 -0500

    chore: add in main page

---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+import { RouterLink } from 'vue-router'
+
+
+<template>
+    <nav role="navigation">
+        <div class="group">
+            <div class="item">
+                <img alt="Linkerd logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+            </div>
+            <div class="v-item">
+                <p>Linkerd</p>
+                <p>Linked. Everywhere.</p>
+            </div>
+        </div>
+        <div class="group">
+            <RouterLink class="router-link" to="/login">Login</RouterLink>
+            <RouterLink class="router-link" to="/signup">Signup</RouterLink>        
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+ nav {
+    margin: 0 auto;
+    background-color: white;
+    font-size: 3rem;
+    line-height:0.9em;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+    border-bottom: solid black 4px;
+
+    position:fixed;
+    width: 100%;
+    top: 0;
+    overflow: hidden;
+
+    z-index:1000;
+ }
+
+ .group {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+ }
+
+ .v-item {
+    display: flex;
+    flex-direction: column;
+ }
+
+.router-link {
+    color:black;
+}
+
+.router-link:first-child {
+    margin-right: 0.25em;
+    border-right: solid black 2px;
+    padding-right: 0.25em;
+}
+.router-link:hover{
+    background-color:black;
+    color:white;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="greetings">
+    <p>
+      You are an individual and have one story, why do your devices not know
+      your story?
+    </p>
+
+    <p>
+      For each one you have to redefine, share, copy, import to resume your
+      story.
+    </p>
+
+    <p>Linkerd solves this problem starting with your browsers.</p>
+  </div>
+</template>
+
+<style scoped>
+  p {
+    font-size:5rem;
+    line-height: 6rem;
+    max-width: 1028px;
+    margin:auto;
+    padding-bottom: 9rem;
+  }
+
+</style>
